042bff4b1e531f5654ec763165d8af59f79f8569
Alhambren <alhambren@gmail.com>
2025-06-13 13:15:45 +1200

    chore: remove obsolete backup and document Vercel deploy

--- src_backup_20250606_wallet_working/config/index.tsx
+++
@@ -1,9 +0,0 @@
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { base } from '@reown/appkit/networks'
-
-const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || '';
-
-export const config = new WagmiAdapter({
-  networks: [base],
-  projectId,
-}); 
